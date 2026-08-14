9fef3c81894e9f893bd10336ed7d50c0873f83ed
SashaKulemin <sashakulemin2026@gmail.com>
2026-08-14 12:38:23 +0400

    create DataBaseSettings

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,18 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class DataBaseSettings(BaseModel):
+    user: str
+    password: str
+    host: str
+    port: int
+    name: str
+
 
 class Settings(BaseSettings):
+    db: DataBaseSettings
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_prefix="APP_CONFIG__",
