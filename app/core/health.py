03db237118eae2f8e3fc72827590b3616e1a3ac7
SashaKulemin <sashakulemin2026@gmail.com>
2026-08-15 09:50:25 +0400

    create health handler

---
+++ app/core/health.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+
+
+router = APIRouter(
+    tags=["Проверка, что приложение работает"]
+)
+
+
+@router.get("/health")
+async def helath_handler() -> dict:
+    return {"status": "ok"}
