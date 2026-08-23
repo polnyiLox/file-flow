3c019baeef3181d43fc1f8a6414136d4b6f964e1
SashaKulemin <sashakulemin2026@gmail.com>
2026-08-23 11:27:13 +0400

    Connect application to MongoDB

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.db import mongodb_client
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    await mongodb_client.connect()
+    try:
+        yield
+    finally:
+        await mongodb_client.close()
+
+
+app = FastAPI(lifespan=lifespan)
