4a8a476f44294796ff6cf5047c3b92a7160c52e2
SashaKulemin <sashakulemin2026@gmail.com>
2026-08-23 11:28:21 +0400

    Configure CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from app.api.routers import v1_router
+from app.core.config import settings
 from app.db import mongodb_client
 from app.exceptions import AppError
 
@@ -21,6 +23,14 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(v1_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.middleware.allow_origins,
+    allow_methods=settings.middleware.allow_methods,
+    allow_headers=settings.middleware.allow_headers,
+    allow_credentials=settings.middleware.allow_credentials,
+)
 
 
 @app.exception_handler(AppError)
