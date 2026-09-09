b447c534689df903ca8700cfd64a1d7c50c54440
SashaKulemin <sashakulemin2026@gmail.com>
2026-09-09 14:18:27 +0400

    implement RabbitMQ consumer and Kafka producer

--- app/broker/rabbitmq/__init__.py
+++ app/broker/rabbitmq/__init__.py
@@ -1,10 +1,1 @@
-from .client import RabbitMQClient
-from .exchanges import RabbitMQExchanges
-from .queues import RabbitMQQueues
-
-
-__all__ = [
-    "RabbitMQClient",
-    "RabbitMQExchanges",
-    "RabbitMQQueues",
-]
+from .consumer import RabbitMQConsumer
